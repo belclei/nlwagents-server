a14e82ca09f4815098d0c9b2a636258380853d50
belclei <belclei@gmail.com>
2025-07-12 16:30:06 -0300

    fix: CORS configuration

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,9 @@ import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.register(fastifyCors, { origin: 'http://localhost:5173' })
+app.register(fastifyCors, {
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+})
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
